9ce8dfbe691bbf1d9f35407bd30d79d33133d2df
Magnus Leßmann <github@m-lessmann.de>
2018-10-27 12:25:03 +0200

    Deqeueing requests

--- src/main/java/de/fearnixx/t3/teamspeak/query/TS3Connection.java
+++ src/main/java/de/fearnixx/t3/teamspeak/query/TS3Connection.java
@@ -17,7 +17,7 @@ public class TS3Connection implements AutoCloseable {
     public static final int SOCKET_TIMEOUT_MILLIS = Main.getProperty("bot.connection.sotimeout", 500);
     public static final int KEEP_ALIVE_SECS = Main.getProperty("bot.connection.keepalive", 240);
     public static final int KEEP_ALIVE_MILLIS = KEEP_ALIVE_SECS * 1000;
-    public static final int MAX_FAILING_KEEPALIVE = Main.getProperty("bot.connection.max_keepalive", 1);
+    public static final int MAX_FAILING_KEEP_ALIVE = Main.getProperty("bot.connection.max_keepalive", 1);
     public static final float REQ_DELAY_SOT_FACTOR = Main.getProperty("bot.connection.reqdelay", 0.25f);
     public static final int REQUEST_DELAY_MILLIS = (int) Math.ceil(SOCKET_TIMEOUT_MILLIS * REQ_DELAY_SOT_FACTOR);
 
@@ -33,7 +33,7 @@ public class TS3Connection implements AutoCloseable {
     private int requestDelayMillis = 0;
     private int timeoutCount = 0;
     private int keepAliveCount = 0;
-    private final IQueryRequest timeoutRequest =
+    private final IQueryRequest keepAliveRequest =
             IQueryRequest.builder()
                     .command("version")
                     .onDone(event -> {
@@ -52,10 +52,23 @@ public class TS3Connection implements AutoCloseable {
     }
 
     public void read() throws IOException {
-        try {
-            messageReader.read();
-        } catch (SocketTimeoutException timeout) {
-            handleTimeout();
+        while (!messageReader.isClosed()) {
+            try {
+                nextRequest();
+                messageReader.read();
+            } catch (SocketTimeoutException timeout) {
+                handleTimeout();
+            }
+        }
+    }
+
+    private void nextRequest() throws IOException {
+        synchronized (requestQueue) {
+            if (currentRequest == null && requestQueue.peek() != null && requestDelayMillis <= 0) {
+                requestDelayMillis = REQUEST_DELAY_MILLIS;
+                currentRequest = requestQueue.poll();
+                messageWriter.writeMessage(currentRequest);
+            }
         }
     }
 
@@ -63,20 +76,21 @@ public class TS3Connection implements AutoCloseable {
         synchronized (requestQueue) {
             if (requestDelayMillis > 0)
                 requestDelayMillis = requestDelayMillis - SOCKET_TIMEOUT_MILLIS;
-        }
 
-        int timeoutTime = ++timeoutCount * SOCKET_TIMEOUT_MILLIS;
-        if (timeoutTime >= KEEP_ALIVE_MILLIS) {
-            if (keepAliveCount++ > MAX_FAILING_KEEPALIVE) {
-                log.severe("Connection lost - Read timed out");
-                close();
+            int timeoutTime = ++timeoutCount * SOCKET_TIMEOUT_MILLIS;
+            if (timeoutTime >= KEEP_ALIVE_MILLIS) {
+                if (keepAliveCount++ > MAX_FAILING_KEEP_ALIVE) {
+                    log.severe("Connection lost - Read timed out");
+                    close();
 
-            } else {
-                log.finest("Sending keepalive");
-                keepAliveCount = 0;
-                sendRequest(timeoutRequest);
+                } else if (currentRequest == null){
+                    log.finest("Sending keepalive");
+                    keepAliveCount = 0;
+                    requestQueue.add(keepAliveRequest);
+                }
             }
         }
+
     }
 
     protected void sendRequest(IQueryRequest request) {
