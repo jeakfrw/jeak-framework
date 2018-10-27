75ecec93067fcb603502f68523f73be3a34f1991
Magnus Leßmann <github@m-lessmann.de>
2018-10-27 19:24:00 +0200

    Extract some methods

--- src/main/java/de/fearnixx/t3/teamspeak/Server.java
+++ src/main/java/de/fearnixx/t3/teamspeak/Server.java
@@ -53,64 +53,8 @@ public class Server implements IServer {
             injectService.injectInto(mainConnection);
             mainConnection.initialize(socket.getInputStream(), socket.getOutputStream());
 
-            mainConnection.sendRequest(
-                    IQueryRequest.builder()
-                            .command("use")
-                            .addOption(Integer.toString(instID))
-                            .onError(answer -> {
-                                log.severe("Failed to use desired instance: ", answer.getError().getMessage());
-                                shutdown();
-                            })
-                            .build()
-            );
-
-            mainConnection.sendRequest(
-                    IQueryRequest.builder().command("login")
-                            .addOption(user)
-                            .addOption(pass)
-                            .onError(answer -> {
-                                log.severe("Failed to login to server: ", answer.getError().getMessage());
-                                shutdown();
-                            })
-                            .build()
-            );
-
-            final String servernotifyregisterCommand = "servernotifyregister";
-            mainConnection.sendRequest(
-                    IQueryRequest.builder()
-                            .command(servernotifyregisterCommand)
-                            .addKey("event", "server")
-                            .build()
-            );
-
-            mainConnection.sendRequest(
-                    IQueryRequest.builder()
-                            .command(servernotifyregisterCommand)
-                            .addKey("event", "channel")
-                            .addKey("id", "0")
-                            .build()
-            );
-
-            mainConnection.sendRequest(
-                    IQueryRequest.builder()
-                            .command(servernotifyregisterCommand)
-                            .addKey("event", "textserver")
-                            .build()
-            );
-
-            mainConnection.sendRequest(
-                    IQueryRequest.builder()
-                            .command(servernotifyregisterCommand)
-                            .addKey("event", "textchannel")
-                            .build()
-            );
-
-            mainConnection.sendRequest(
-                    IQueryRequest.builder()
-                            .command(servernotifyregisterCommand)
-                            .addKey("event", "textprivate")
-                            .build()
-            );
+            login(user, pass, instID);
+            subscribeToEvents();
 
             connectionThread = new Thread(mainConnection);
             connectionThread.start();
@@ -120,13 +64,71 @@ public class Server implements IServer {
         }
     }
 
-    /* * * RUNTIME CONTROL * * */
+    private void login(String user, String pass, int instID) {
+        mainConnection.sendRequest(
+                IQueryRequest.builder()
+                        .command("use")
+                        .addOption(Integer.toString(instID))
+                        .onError(answer -> {
+                            log.severe("Failed to use desired instance: ", answer.getError().getMessage());
+                            shutdown();
+                        })
+                        .build()
+        );
 
-    private void onClose(IQueryConnection conn) {
-        if (conn == mainConnection) {
-            shutdown();
-        }
+        mainConnection.sendRequest(
+                IQueryRequest.builder().command("login")
+                        .addOption(user)
+                        .addOption(pass)
+                        .onError(answer -> {
+                            log.severe("Failed to login to server: ", answer.getError().getMessage());
+                            shutdown();
+                        })
+                        .build()
+        );
     }
+
+    private void subscribeToEvents() {
+        final String servernotifyregisterCommand = "servernotifyregister";
+        final String eventKey = "event";
+        mainConnection.sendRequest(
+                IQueryRequest.builder()
+                        .command(servernotifyregisterCommand)
+                        .addKey(eventKey, "server")
+                        .build()
+        );
+
+        mainConnection.sendRequest(
+                IQueryRequest.builder()
+                        .command(servernotifyregisterCommand)
+                        .addKey(eventKey, "channel")
+                        .addKey("id", "0")
+                        .build()
+        );
+
+        mainConnection.sendRequest(
+                IQueryRequest.builder()
+                        .command(servernotifyregisterCommand)
+                        .addKey(eventKey, "textserver")
+                        .build()
+        );
+
+        mainConnection.sendRequest(
+                IQueryRequest.builder()
+                        .command(servernotifyregisterCommand)
+                        .addKey(eventKey, "textchannel")
+                        .build()
+        );
+
+        mainConnection.sendRequest(
+                IQueryRequest.builder()
+                        .command(servernotifyregisterCommand)
+                        .addKey(eventKey, "textprivate")
+                        .build()
+        );
+    }
+
+    /* * * RUNTIME CONTROL * * */
 
     public void shutdown() {
         if (connectionThread != null) {
