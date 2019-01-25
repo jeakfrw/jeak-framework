0ef9b7016a35615ae6de37f7975b12c5993f88e5
Magnus Leßmann <github@m-lessmann.de>
2019-01-25 23:35:02 +0100

    Add scaffolding for test-plugins

---
+++ src/api/java/de/fearnixx/jeak/test/AbstractTestPlugin.java
@@ -0,0 +1,43 @@
+package de.fearnixx.jeak.test;
+
+import java.util.*;
+
+/**
+ * As JUnit-testing is not compatible with high-level testing for the framework without having to mock the major part,
+ * an alternative solution is available:
+ * Test plugins are plugins that extend this abstract class.
+ * The class tracks the instances created while the framework is running.
+ * This way, the framework can be connected to a real test instance of TS3 in order to run tests.
+ *
+ * On shutdown, the framework then retrieves the registered tests and their results.
+ */
+public abstract class AbstractTestPlugin {
+
+    private static final List<AbstractTestPlugin> testPlugins = new LinkedList<>();
+
+    public static final List<AbstractTestPlugin> getTestPlugins() {
+        return Collections.unmodifiableList(testPlugins);
+    }
+
+    private final Map<String, Boolean> results = new HashMap<>();
+
+    protected AbstractTestPlugin() {
+        testPlugins.add(this);
+    }
+
+    protected void addTest(String testName) {
+        results.put(testName, false);
+    }
+
+    protected void fail(String testName) {
+        results.put(testName, false);
+    }
+
+    protected void success(String testName) {
+        results.put(testName, true);
+    }
+
+    public Map<String, Boolean> getResults() {
+        return results;
+    }
+}
---
+++ src/test/java/de/fearnixx/jeak/test/plugin/TestMain.java
@@ -0,0 +1,11 @@
+package de.fearnixx.jeak.test.plugin;
+
+import de.fearnixx.jeak.Main;
+
+public class TestMain {
+
+    public static void main(String[] args) {
+        System.setProperty("bot.enableTests", "true");
+        Main.main(args);
+    }
+}
