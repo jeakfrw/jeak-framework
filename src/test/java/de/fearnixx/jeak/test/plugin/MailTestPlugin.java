250c15e945f8099c298eb28b037a874c321cb0ea
Magnus Leßmann <github@m-lessmann.de>
2019-05-04 17:18:06 +0200

    Make mail-tests opt-in

--- src/test/java/de/fearnixx/jeak/test/plugin/MailTestPlugin.java
+++ src/test/java/de/fearnixx/jeak/test/plugin/MailTestPlugin.java
@@ -1,11 +1,14 @@
 package de.fearnixx.jeak.test.plugin;
 
+import de.fearnixx.jeak.Main;
 import de.fearnixx.jeak.event.bot.IBotStateEvent;
 import de.fearnixx.jeak.reflect.*;
 import de.fearnixx.jeak.service.mail.IAttachment;
 import de.fearnixx.jeak.service.mail.IMail;
 import de.fearnixx.jeak.service.mail.ITransportUnit;
 import de.fearnixx.jeak.test.AbstractTestPlugin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -13,6 +16,11 @@ import java.io.IOException;
 
 @JeakBotPlugin(id = "mailtest")
 public class MailTestPlugin extends AbstractTestPlugin {
+
+    private static final String PROP_TEST_RECIPIENT = "jeak.test.mailTest.recipient";
+    private static final String TEST_RECIPIENT = Main.getProperty(PROP_TEST_RECIPIENT, null);
+
+    private static final Logger logger = LoggerFactory.getLogger(MailTestPlugin.class);
 
     @Inject
     @TransportUnit(name = "test")
@@ -23,30 +31,36 @@ public class MailTestPlugin extends AbstractTestPlugin {
     private File dummyConf;
 
     public MailTestPlugin() {
-        addTest("send_mail");
-        addTest("send_mail_attached");
+        if (TEST_RECIPIENT != null) {
+            addTest("send_mail");
+            addTest("send_mail_attached");
+        } else {
+            logger.warn("Not running tests. Missing recipient. ({})", PROP_TEST_RECIPIENT);
+        }
     }
 
     @Listener
     public void onConnected(IBotStateEvent.IConnectStateEvent.IPostConnect event) {
-        IMail message = IMail.builder()
-                .addSubjectText("Yay! It works!")
-                .addText("This is a test-email!")
-                .to("technik@fearnixx.de")
-                .build();
+        if (TEST_RECIPIENT != null) {
+            IMail message = IMail.builder()
+                    .addSubjectText("Yay! It works!")
+                    .addText("This is a test-email!")
+                    .to(TEST_RECIPIENT)
+                    .build();
 
-        transportUnit.dispatch(message);
-        success("send_mail");
+            transportUnit.dispatch(message);
+            success("send_mail");
 
-        writeDummy();
-        IMail attachedMessage = IMail.builder()
-                .addSubjectText("Yay! This works too!")
-                .addText("This example is for attachments.")
-                .to("technik@fearnixx.de")
-                .attach(IAttachment.builder().fromFile("MyAttachment.json", dummyConf))
-                .build();
-        transportUnit.dispatch(attachedMessage);
-        success("send_mail_attached");
+            writeDummy();
+            IMail attachedMessage = IMail.builder()
+                    .addSubjectText("Yay! This works too!")
+                    .addText("This example is for attachments.")
+                    .to("technik@fearnixx.de")
+                    .attach(IAttachment.builder().fromFile("MyAttachment.json", dummyConf))
+                    .build();
+            transportUnit.dispatch(attachedMessage);
+            success("send_mail_attached");
+        }
     }
 
     private void writeDummy() {
--- src/test/java/de/fearnixx/jeak/test/plugin/NotificationTestPlugin.java
+++ src/test/java/de/fearnixx/jeak/test/plugin/NotificationTestPlugin.java
@@ -1,5 +1,6 @@
 package de.fearnixx.jeak.test.plugin;
 
+import de.fearnixx.jeak.Main;
 import de.fearnixx.jeak.event.IQueryEvent;
 import de.fearnixx.jeak.event.bot.IBotStateEvent;
 import de.fearnixx.jeak.profile.IProfileService;
@@ -10,6 +11,8 @@ import de.fearnixx.jeak.reflect.Listener;
 import de.fearnixx.jeak.service.notification.*;
 import de.fearnixx.jeak.teamspeak.PropertyKeys;
 import de.fearnixx.jeak.test.AbstractTestPlugin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Optional;
 
@@ -17,6 +20,10 @@ import java.util.Optional;
 public class NotificationTestPlugin extends AbstractTestPlugin {
 
     private static final String RECIPIENT_UID = "GANC6dTbew+a3A2h/8c5CGJXzsE=";
+    private static final String PROP_TEST_RECIPIENT = "jeak.test.notifyTest.recipient";
+    private static final String NOTIFY_TEST_RECIPIENT = Main.getProperty(PROP_TEST_RECIPIENT, null);
+
+    private static final Logger logger = LoggerFactory.getLogger(NotificationTestPlugin.class);
 
     @Inject
     private INotificationService notificationService;
@@ -26,29 +33,36 @@ public class NotificationTestPlugin extends AbstractTestPlugin {
 
     public NotificationTestPlugin() {
         super();
-        addTest("notficiationTest_profileRetrieved");
-        addTest("notficiationTest_messageDispatched");
         addTest("notificationTest_liveMessageDispatched");
+
+        if (NOTIFY_TEST_RECIPIENT != null) {
+            addTest("notficiationTest_profileRetrieved");
+            addTest("notficiationTest_messageDispatched");
+        } else {
+            logger.warn("Not running mail tests as no recipient has been defined. ({})", PROP_TEST_RECIPIENT);
+        }
     }
 
     @Listener
     public void onConnected(IBotStateEvent.IConnectStateEvent.IPostConnect events) {
-        Optional<IUserProfile> optProfile = profileService.getOrCreateProfile(RECIPIENT_UID);
-        if (!optProfile.isPresent()) {
-            fail("notficiationTest_profileRetrieved");
-            return;
-        }
+        if (NOTIFY_TEST_RECIPIENT != null) {
+            Optional<IUserProfile> optProfile = profileService.getOrCreateProfile(RECIPIENT_UID);
+            if (!optProfile.isPresent()) {
+                fail("notficiationTest_profileRetrieved");
+                return;
+            }
 
-        optProfile.get().setOption(SendMailChannel.MAIL_ADDRESS_PROFILE_OPTION, "technik@fearnixx.de");
-        INotification notification = INotification.builder()
-                .addRecipient(RECIPIENT_UID)
-                .urgency(Urgency.ALERT)
-                .lifespan(Lifespan.FOREVER)
-                .summary("[TESTSYSTEM] Mail notification test.")
-                .longText("This is a test notification.")
-                .build();
-        notificationService.dispatch(notification);
-        success("notficiationTest_messageDispatched");
+            optProfile.get().setOption(SendMailChannel.MAIL_ADDRESS_PROFILE_OPTION, PROP_TEST_RECIPIENT);
+            INotification notification = INotification.builder()
+                    .addRecipient(RECIPIENT_UID)
+                    .urgency(Urgency.ALERT)
+                    .lifespan(Lifespan.FOREVER)
+                    .summary("[TESTSYSTEM] Mail notification test.")
+                    .longText("This is a test notification.")
+                    .build();
+            notificationService.dispatch(notification);
+            success("notficiationTest_messageDispatched");
+        }
     }
 
     @Listener
