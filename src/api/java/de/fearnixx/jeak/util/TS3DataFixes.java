32145cfbed7fa1aca5ef0f663472c5dffa4d6a9c
Magnus Leßmann <github@m-lessmann.de>
2019-05-09 14:42:16 +0200

    Apply icon ID fixes to clients too!

---
+++ src/api/java/de/fearnixx/jeak/util/TS3DataFixes.java
@@ -0,0 +1,43 @@
+package de.fearnixx.jeak.util;
+
+import de.fearnixx.jeak.teamspeak.PropertyKeys;
+import de.fearnixx.jeak.teamspeak.data.IDataHolder;
+
+import java.util.Optional;
+
+/**
+ * Data inconsistencies in the query responses may be worked-around in the framework.
+ * This class contains a selection of those fixes.
+ * <p>
+ * Not included are semantic issues like:
+ * * {@link de.fearnixx.jeak.event.IQueryEvent.INotification.IClientEnter} using "ctid" instead of "cid" for the channel ID. (Fix not included in framework.)
+ * * {@link de.fearnixx.jeak.event.IQueryEvent.INotification.IChannelEdited}s information being missing for edited descriptions and password. (Fix included in framework.)
+ */
+public class TS3DataFixes {
+
+    /**
+     * It appears that TS3 tends to fail reading icon IDs correctly.
+     * Icon IDs are supposed to be a CRC32 checksum. However, TS3 very frequently returns negative numbers for those.
+     * Investigation showed TS3 reading the internal integer as signed whereas it really is unsigned.
+     * <p>
+     * This will fix this issue by re-reading negative icon IDs as unsigned numbers.
+     *
+     * @param dataHolder   The {@link IDataHolder} this fix should be applied to.
+     * @param propertyName The property under which the icon ID is found.
+     * @see PropertyKeys.Client#ICON_ID
+     * @see PropertyKeys.Channel#ICON_ID
+     */
+    public static void ICONS_INVALID_CRC32(IDataHolder dataHolder, String propertyName) {
+        Optional<String> optIconID = dataHolder.getProperty(propertyName);
+        optIconID.ifPresent(iconId -> {
+            if (iconId.startsWith("-")) {
+                int idFromTS = Integer.parseInt(iconId);
+                long realID = Integer.toUnsignedLong(idFromTS);
+                dataHolder.setProperty(
+                        PropertyKeys.Channel.ICON_ID,
+                        Long.toString(realID)
+                );
+            }
+        });
+    }
+}
