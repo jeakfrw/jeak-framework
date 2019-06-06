81d7c5575460c9de79eae58b9744ec59697a3d22
Magnus Leßmann <github@m-lessmann.de>
2019-06-06 09:46:14 +0200

    Add option to lazily retrieve permIDs when list failed

--- src/main/java/de/fearnixx/jeak/service/permission/teamspeak/PermIdCache.java
+++ src/main/java/de/fearnixx/jeak/service/permission/teamspeak/PermIdCache.java
@@ -7,11 +7,15 @@ import de.fearnixx.jeak.reflect.Listener;
 import de.fearnixx.jeak.service.task.ITask;
 import de.fearnixx.jeak.service.task.ITaskService;
 import de.fearnixx.jeak.teamspeak.IServer;
+import de.fearnixx.jeak.teamspeak.data.IDataHolder;
+import de.fearnixx.jeak.teamspeak.query.BlockingRequest;
 import de.fearnixx.jeak.teamspeak.query.IQueryRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
@@ -47,7 +51,10 @@ public class PermIdCache {
         server.getConnection().sendRequest(
                 IQueryRequest.builder()
                         .command("permissionlist")
-                        .onError(err -> logger.error("Failed to refresh permission id cache! {} - {}", err.getErrorCode(), err.getErrorMessage()))
+                        .onError(err -> {
+                            logger.error("Failed to refresh permission id cache! {} - {}", err.getErrorCode(), err.getErrorMessage());
+                            logger.warn("Please grant \"b_serverinstance_permission_list\" to resolve this issue!");
+                        })
                         .onSuccess(this::parseAnswer)
                         .build());
     }
@@ -67,10 +74,58 @@ public class PermIdCache {
     }
 
     public synchronized Integer getPermIdFor(String permSID) {
+        Objects.requireNonNull(permSID, "PermSID may not be null!");
+        if (permSID.trim().isEmpty()) {
+            throw new IllegalArgumentException("PermSID may not be empty!");
+        }
+
         Integer permId = internalCache.getOrDefault(permSID, null);
         if (permId == null) {
-            throw new IllegalArgumentException("Unknown permSID: " + permSID);
+            logger.info("Cache-miss for \"{}\". Attempting lazy retrieval.", permSID);
+            Optional<Integer> optPermId = lazilyGetPermId(permSID);
+
+             permId = optPermId.orElseThrow(() -> new IllegalArgumentException("Unknown permSID: " + permSID));
+             logger.debug("Updating cache: {} -> {}", permSID, permId);
+             internalCache.put(permSID, permId);
         }
         return permId;
+    }
+
+    private Optional<Integer> lazilyGetPermId(String permSid) {
+        IQueryRequest request = IQueryRequest.builder()
+                .command("permidgetbyname")
+                .addKey("permsid", permSid)
+                .build();
+
+        BlockingRequest bRequest = new BlockingRequest(request);
+        if (bRequest.waitForCompletion()) {
+            IQueryEvent.IAnswer answer = bRequest.getAnswer();
+
+            if (answer.getErrorCode() == 0) {
+                List<IDataHolder> chain = answer.getDataChain();
+                for (IDataHolder holder : chain) {
+                    String dataSID = holder.getProperty("permsid").orElse("_");
+                    if (dataSID.equals(permSid)) {
+                        Integer permid = holder.getProperty("permid")
+                                .map(Integer::parseInt)
+                                .orElse(-1);
+
+                        if (permid > 0) {
+                            return Optional.of(permid);
+                        } else {
+                            logger.warn("Failed to retrieve permID from answer!");
+                        }
+                    } else {
+                        logger.debug("Skipping result SID: {}", dataSID);
+                    }
+                }
+            } else {
+                logger.warn("Non-OK return code for permID lookup: {} - {}", answer.getErrorCode(), answer.getErrorMessage());
+            }
+        } else {
+            logger.warn("Could not complete blocking request for permID lookup.");
+        }
+
+        return Optional.empty();
     }
 }
