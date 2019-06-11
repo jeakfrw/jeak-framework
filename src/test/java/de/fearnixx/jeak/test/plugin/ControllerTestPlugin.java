9adb72a97dd3a2d6f470f374f99776044fcda77e
fwellers <finn42@gmx.de>
2019-06-11 19:51:39 +0200

    Fix code according to comment in #30
    Add expected success and catch exception

--- src/test/java/de/fearnixx/jeak/test/plugin/ControllerTestPlugin.java
+++ src/test/java/de/fearnixx/jeak/test/plugin/ControllerTestPlugin.java
@@ -6,6 +6,7 @@ import de.fearnixx.jeak.reflect.Inject;
 import de.fearnixx.jeak.reflect.JeakBotPlugin;
 import de.fearnixx.jeak.reflect.Listener;
 import de.fearnixx.jeak.service.controller.IRestControllerService;
+import de.fearnixx.jeak.service.controller.RegisterControllerException;
 import de.fearnixx.jeak.service.controller.testImpls.SecondTestController;
 import de.fearnixx.jeak.service.controller.testImpls.TestController;
 import de.fearnixx.jeak.teamspeak.cache.IDataCache;
@@ -16,33 +17,21 @@ import de.mlessmann.confort.api.IConfig;
 public class ControllerTestPlugin extends AbstractTestPlugin {
 
     @Inject
-    @Config
-    public IConfig configRef;
-
-    @Inject
-    public IDataCache dataCache;
-
-    @Inject
     IRestControllerService restControllerService;
 
 
     public ControllerTestPlugin() {
         super();
-        addTest("test_injected_configRef");
-        addTest("test_injected_dataCache");
     }
 
     @Listener
     public void onInitialize(IBotStateEvent.IInitializeEvent event) {
-        if (configRef != null) {
-            success("test_injected_configRef");
-        }
-
-        if (dataCache != null) {
-            success("test_injected_dataCache");
-        }
         restControllerService.registerController(TestController.class, new TestController());
-        restControllerService.registerController(SecondTestController.class, new SecondTestController());
+        try {
+            restControllerService.registerController(SecondTestController.class, new SecondTestController());
+        } catch (RegisterControllerException e) {
+            success("Should fail to register a duplicated controller");
+        }
 
     }
 }
