c9b026d77c6d5eacbe3a01ac185c9f7987ccb35b
finnWellers <finn42@gmx.de>
2019-06-11 23:45:37 +0200

    Add new class for TokenScope

---
+++ src/main/java/de/fearnixx/jeak/service/token/TokenScope.java
@@ -0,0 +1,51 @@
+package de.fearnixx.jeak.service.token;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.Set;
+
+/**
+ * pluginId/
+ * pluginId/Controller/
+ * pluginId/Controller/method1
+ */
+public class TokenScope {
+    private Set<String> scopeSet;
+
+    public TokenScope(Set<String> scopeSet) {
+        this.scopeSet = scopeSet;
+    }
+
+    public void addScope(String scope) {
+        this.scopeSet.add(scope);
+    }
+
+    public Set<String> getScopeSet() {
+        return scopeSet;
+    }
+
+    public boolean isInScope(String scope) {
+        String[] splittedStrings = splitScope(scope);
+        for (int i = 0; i < splittedStrings.length; i++) {
+            Optional<String> optionalShortenedScope = combineStrings(Arrays.copyOf(splittedStrings, splittedStrings.length - i));
+            if (optionalShortenedScope.isPresent()) {
+                String localScope = optionalShortenedScope.get();
+                if (scopeSet.contains(localScope)) {
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    private Optional<String> combineStrings(String ... strings) {
+        return Arrays.stream(strings)
+                .reduce((s, s2) -> s+"/"+s2);
+    }
+
+    private String[] splitScope(String scope) {
+        return scope.split("/");
+    }
+}
